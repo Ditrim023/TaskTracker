982ba5156c04e7bb4becead87b4c3e83c178860e
ditrim023 <krutoguz@cloudwharf.com>
2020-06-26 14:49:12 +0300

    add dependencies and user model

---
+++ src/main/java/com/example/task/tracker/model/TrackerUser.java
@@ -0,0 +1,50 @@
+package com.example.task.tracker.model;
+
+public class TrackerUser {
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public TrackerUser() {
+    }
+
+    public TrackerUser(Long userId, String firstName, String lastName, String email) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
