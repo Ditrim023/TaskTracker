46ba380245a85c0196883022a1575ff1560be459
ditrim023 <krutoguz@cloudwharf.com>
2020-06-29 17:54:32 +0300

    add redme

--- src/main/java/com/example/task/tracker/security/config/ProfileConfig.java
+++ src/main/java/com/example/task/tracker/security/config/ProfileConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,17 +37,11 @@ public class ProfileConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/login", "/registration").permitAll()
-                .antMatchers("/user/**","/users/**","/task/**","/tasks").hasRole("USER")
+                .antMatchers("/user/**", "/users/**", "/task/**", "/tasks").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
 
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                .antMatchers("/css/**", "/images/**");
-    }
 }
